bdcbaddee06b5bdec3984b0d86cca6b638e4f6b3
lemanpashayeva <lmnpasayeva@gmail.com>
2022-10-13 00:46:25 +0400

    second commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 'use strict';
 
-const names = document.getElementById('name');
-const surnames = document.getElementById('lname');
-const age = document.getElementById('age');
-const submit = document.getElementById('submit');
+const input = document.getElementsByTagName('input');
 const tr = document.createElement('tr');
 const td = document.createElement('td');
 const p = document.createElement('p');
-
+const submit = document.getElementById('submit');
 
 submit.addEventListener('click' , function() {
-    this.parentNode.parentNode.parentNode.appendChild(tr);
-        tr.appendChild(td.appendChild(p));
-        p.innerText = names.value;
+    for (let i = 0; i < input.length; i++) {
+        this.parentNode.parentNode.parentNode.appendChild(tr);
+        tr.appendChild(td);
+        td.appendChild(p);
+        p.innerText = input[i].value;
+    }
 })
